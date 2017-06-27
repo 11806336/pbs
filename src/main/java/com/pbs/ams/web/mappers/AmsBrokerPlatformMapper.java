aabb2c6dc8141568b75539344462f8e04c6e1208
TiAmo丶 <TiAmo@TiAmo.local>
2017-06-27 11:07:09 +0800

    还原

--- src/main/java/com/pbs/ams/web/mappers/AmsBrokerPlatformMapper.java
+++ src/main/java/com/pbs/ams/web/mappers/AmsBrokerPlatformMapper.java
@@ -2,9 +2,6 @@ package com.pbs.ams.web.mappers;
 
 import com.pbs.ams.web.model.AmsBrokerPlatform;
 import com.pbs.ams.web.model.AmsBrokerPlatformExample;
-import com.pbs.ams.web.model.AmsMarket;
-import com.pbs.ams.web.model.AmsMarketExample;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -12,25 +9,8 @@ import java.util.List;
  * Created by Administrator on 2017/6/22.
  */
 public interface AmsBrokerPlatformMapper {
-    long countByExample(AmsMarketExample example);
 
-    int deleteByExample(AmsMarketExample example);
+    List<AmsBrokerPlatform> selectBrokerPlatform(AmsBrokerPlatformExample amsBrokerPlatformExample);
 
-    int deleteByPrimaryKey(Long marketId);
-
-    int insert(AmsMarket record);
-
-    int insertSelective(AmsMarket record);
-
-    List<AmsMarket> selectByExample(AmsMarketExample example);
-
-    AmsMarket selectByPrimaryKey(Long marketId);
-
-    int updateByExampleSelective(@Param("record") AmsMarket record, @Param("example") AmsMarketExample example);
-
-    int updateByExample(@Param("record") AmsMarket record, @Param("example") AmsMarketExample example);
-
-    int updateByPrimaryKeySelective(AmsMarket record);
-
-    int updateByPrimaryKey(AmsMarket record);
+    Long selectCountByExample(AmsBrokerPlatformExample amsBrokerPlatformExample);
 }
