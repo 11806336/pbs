008fc4910b98a5b70441637dec3de19316a4b1f5
zhangpengfei <11806336@qq.com>
2017-06-29 15:35:08 +0800

    重写方法

--- src/main/java/com/pbs/ams/web/service/impl/UpmsCompanyServiceImpl.java
+++ src/main/java/com/pbs/ams/web/service/impl/UpmsCompanyServiceImpl.java
@@ -1,119 +1,111 @@
 package com.pbs.ams.web.service.impl;
 
-import com.pbs.ams.common.annotation.BaseService;
-import com.pbs.ams.common.base.BaseServiceImpl;
-import com.pbs.ams.common.db.DataSourceEnum;
-import com.pbs.ams.common.db.DynamicDataSource;
 import com.pbs.ams.web.mappers.UpmsCompanyMapper;
 import com.pbs.ams.web.model.UpmsCompany;
-import com.pbs.ams.web.model.UpmsCompanyExample;
-import com.pbs.ams.web.model.UpmsCompanySnaps;
+import com.pbs.ams.web.model.UpmsCompanySnapshot;
 import com.pbs.ams.web.service.UpmsCompanyService;
 import org.apache.commons.beanutils.PropertyUtils;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
-* UpmsCompanyService实现
+* 基金公司服务实现类
 * Created by ams on 2017/6/20.
 */
 @Service
 @Transactional
-@BaseService
-public class UpmsCompanyServiceImpl extends BaseServiceImpl<UpmsCompany, UpmsCompanyExample> implements UpmsCompanyService {
+public class UpmsCompanyServiceImpl implements UpmsCompanyService {
 
-    private static Logger _log = LoggerFactory.getLogger(UpmsCompanyServiceImpl.class);
+    private static Logger log = LoggerFactory.getLogger(UpmsCompanyServiceImpl.class);
 
     @Autowired
     private UpmsCompanyMapper upmsCompanyMapper;
 
-    @Override
-    public Object getMapper() { return upmsCompanyMapper; }
 
-    /**
-     * 删除操作
-     * @param ids 主键id
-     * @return
-     */
     @Override
-    public int deleteByPrimaryKeys(String ids) {
-        try {
-            if (StringUtils.isBlank(ids)) {
-                return 0;
-            }
-            DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getName());
-            String[] idArray = ids.split("-");
+    public int deleteCompany(List<Long> ids) {
+        if (null != ids && ids.size() >0) {
             int count = 0;
-            for (String idStr : idArray) {
-                if (StringUtils.isBlank(idStr)) {
-                    continue;
-                }
-                Long id = Long.parseLong(idStr);
-                //先查询本条记录
-                Method selectByPrimaryKey =  getMapper().getClass().getDeclaredMethod("selectByPrimaryKey", id.getClass());
-                UpmsCompany upmsCompany = (UpmsCompany) selectByPrimaryKey.invoke( getMapper(), id);
-                //将本条记录放在快照表中
-                if (upmsCompany != null) {//查到数据后再做新增和删除操作
-                    UpmsCompanySnaps snaps = new UpmsCompanySnaps();
-                    PropertyUtils.copyProperties(snaps, upmsCompany);
-                    snaps.setSnapsTime(System.currentTimeMillis());
-//                    Method insertSelective =  getMapper().getClass().getDeclaredMethod("insertSelective", snaps.getClass());
-//                    Object t = insertSelective.invoke( getMapper(), snaps);
-                    int insertResult = upmsCompanyMapper.insertSnapshotSelective(snaps);
-                    if (insertResult == 0) {
-                        new RuntimeException();
+            for (long id : ids) {
+                //先做查询再去删除原表数据和插入快照
+                UpmsCompany company = upmsCompanyMapper.getCompany(id);
+                if (company != null) {
+                    UpmsCompanySnapshot snapshot = new UpmsCompanySnapshot();
+                    try {
+                        PropertyUtils.copyProperties(snapshot, company);
+                        //向快照表插入数据
+                        int snapshotResult = upmsCompanyMapper.insertSnapshot(snapshot);
+                        count += upmsCompanyMapper.deleteCompanyById(id);
+                    } catch (IllegalAccessException e) {//checkException
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
                     }
-                    Method deleteByPrimaryKey =  getMapper().getClass().getDeclaredMethod("deleteByPrimaryKey", id.getClass());
-                    Object result = deleteByPrimaryKey.invoke( getMapper(), id);
-                    count += Integer.parseInt(String.valueOf(result));
                 }
             }
             return count;
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        DynamicDataSource.clearDataSource();
         return 0;
     }
 
-    /**
-     * 修改公司
-     * @param upmsCompany
-     * @return
-     */
     @Override
-    public int updateByPrimaryKeySelective(UpmsCompany upmsCompany) {
-        try {
-            DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getName());
-            int result = 0;
-            //先查询本条记录
-            Long id = upmsCompany.getCompanyId();
-            Method selectByPrimaryKey =  getMapper().getClass().getDeclaredMethod("selectByPrimaryKey", id.getClass());
-            UpmsCompany companyOld = (UpmsCompany) selectByPrimaryKey.invoke( getMapper(), id);
-            //将本条记录放在快照表中
-            if (companyOld != null) {//查到数据后再做新增和删除操作
-                UpmsCompanySnaps snaps = new UpmsCompanySnaps();
-                PropertyUtils.copyProperties(snaps, companyOld);
-                snaps.setSnapsTime(System.currentTimeMillis());
-                int insertResult = upmsCompanyMapper.insertSnapshotSelective(snaps);
-                if (insertResult == 0) {
-                    new RuntimeException();
+    public int insertCompany(UpmsCompany company) {
+        return upmsCompanyMapper.insertCompany(company);
+    }
+
+    @Override
+    public UpmsCompany getCompany(Long id) {
+        return upmsCompanyMapper.getCompany(id);
+    }
+
+    @Override
+    public int updateCompany(UpmsCompany company) {
+        if (null != company) {
+            //先做查询再去更新原表数据和插入快照
+            UpmsCompany oldCompany = upmsCompanyMapper.getCompany(company.getCompanyId());
+            if (null != oldCompany) {
+                UpmsCompanySnapshot snapshot = new UpmsCompanySnapshot();
+                try {
+                    PropertyUtils.copyProperties(snapshot, oldCompany);
+                    //向快照表插入数据
+                    int snapshotResult = upmsCompanyMapper.insertSnapshot(snapshot);
+                    if (snapshotResult > 0) {//当插入成功后再更新原数据
+                        return upmsCompanyMapper.updateCompany(company);
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
                 }
-//                Method updateByPrimaryKeySelective =  getMapper().getClass().getDeclaredMethod("updateByPrimaryKeySelective", upmsCompany.getClass());
-//                result = Integer.parseInt(String.valueOf(updateByPrimaryKeySelective.invoke( getMapper(), upmsCompany)));
-                result = upmsCompanyMapper.updateByPrimaryKeySelective(upmsCompany);
             }
-            return result;
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        DynamicDataSource.clearDataSource();
         return 0;
+    }
+
+    @Override
+    public List<UpmsCompany> listCompanies(Map<String, Object> params) {
+        return upmsCompanyMapper.listCompanies(params);
+    }
+
+    @Override
+    public int insertSnapshot(UpmsCompanySnapshot snapshot) {
+        return upmsCompanyMapper.insertSnapshot(snapshot);
+    }
+
+    @Override
+    public int countCompany(Map<String, Object> params) {
+        return upmsCompanyMapper.countCompany(params);
     }
 }
