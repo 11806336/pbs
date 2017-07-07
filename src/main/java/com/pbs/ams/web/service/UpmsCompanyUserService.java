c8b6f6f5ded9877f80e8c7e3e6999d611d02681c
luyunchuan <992036766@qq.com>
2017-07-07 15:43:59 +0800

    添加公司用户关系表服务

---
+++ src/main/java/com/pbs/ams/web/service/UpmsCompanyUserService.java
@@ -0,0 +1,21 @@
+package com.pbs.ams.web.service;
+
+import com.pbs.ams.web.model.UpmsCompany;
+import com.pbs.ams.web.model.UpmsCompanyUser;
+
+import java.util.List;
+
+/**
+* 基金公司与用户关系服务接口
+* Created by ams on 2017/6/20.
+*/
+public interface UpmsCompanyUserService {
+
+
+    /**
+     * get company by by current user`s id
+     * @param userId
+     * @return List<UpmsCompany>
+     */
+    List<UpmsCompanyUser> getCompaniesByUserId(Long userId);
+}
