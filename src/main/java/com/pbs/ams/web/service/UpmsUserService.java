1a15837a3a2ad9a7878e7e8367a4cb957ae88829
luyunchuan <992036766@qq.com>
2017-07-13 18:18:09 +0800

    产品中添加用户关联

--- src/main/java/com/pbs/ams/web/service/UpmsUserService.java
+++ src/main/java/com/pbs/ams/web/service/UpmsUserService.java
@@ -4,15 +4,19 @@ import com.pbs.ams.common.base.BaseService;
 import com.pbs.ams.web.model.UpmsUser;
 import com.pbs.ams.web.model.UpmsUserExample;
 
+import java.util.List;
+
 /**
 * UpmsUserService接口
 * Created by ams on 2017/3/20.
 */
 
 public interface UpmsUserService extends BaseService<UpmsUser, UpmsUserExample> {
-//
+
     UpmsUser insert2(UpmsUser upmsUser);
-//
+
+    List<UpmsUser> selectUsersById(List<Long> userIds);
+
 //    UpmsUser selectByPrimaryKey(Integer id);
 
 //    List<UpmsUser> selectByExample(UpmsUserExample example);
